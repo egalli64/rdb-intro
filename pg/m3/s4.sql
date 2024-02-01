08b685f32c890e827ea9ca903f56928e49552ec4
Emanuele <egalli64@gmail.com>
2024-02-01 18:04:50 +0100

    more comments

--- pg/m3/s4.sql
+++ pg/m3/s4.sql
@@ -1,11 +1,16 @@
 -- examples on date functions
 
--- current date and time
+-- current date
 select current_date;
+
+-- current date-time with time zone
 select current_timestamp, now();
+
+-- current date-time (no time zone)
 select localtimestamp, current_timestamp::timestamp;
-select current_time, localtime;
-select current_timestamp, localtimestamp;
+
+-- current date-time, with and  without time zone
+select current_time, current_time::time, localtime;
 
 -- operators -, +
 select current_date - 1 as yesterday, current_date + 1 as tomorrow;
