dc4f8f4ae10147d37fec0152c16779880ab2719a
Emanuele <egalli64@gmail.com>
2024-03-31 21:59:00 +0200

    review

--- pg/sql50/2_basic_join/easy_0197.sql
+++ pg/sql50/2_basic_join/easy_0197.sql
@@ -10,9 +10,21 @@
  * | recordDate    | date    |
  * | temperature   | int     |
  * +---------------+---------+
- * 
- * Find the id for registrations with with higher temperatures compared to its previous day
  */
+
+-- set up for testing on the leet schema
+create table weather (
+	id serial primary key,
+	recordDate date unique,
+	temperature int
+);
+
+insert into Weather (recordDate, temperature) values ('2015-01-01', 10), ('2015-01-02', 25), ('2015-01-03', 20), ('2015-01-04', 30);
+
+-- Find the id for registrations with with higher temperatures compared to its previous day
 select w.id
 from weather w cross join weather y
 where w.recordDate = y.recordDate + 1 and w.temperature > y.temperature;
+
+-- cleanup of the leet schema
+drop table weather;
