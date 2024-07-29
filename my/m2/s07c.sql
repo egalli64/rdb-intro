371c2aafee89290f5994a4d795b04ae236c0fa00
Emanuele <egalli64@gmail.com>
2024-07-29 11:14:16 +0200

    inner join emulation by union

---
+++ my/m2/s07c.sql
@@ -0,0 +1,38 @@
+/*
+    Introduction to SQL (on MySQL)
+    
+    https://github.com/egalli64/hron
+
+	Using union to emulate an inner join
+ */
+
+select region_id, name
+from region;
+
+-- 1|Europe
+-- 2|Americas
+-- 3|Asia
+-- 4|Middle East and Africa
+
+	select region_id as r_id, 'Europe' as region, country_id, name
+	from country
+	where region_id = 1
+union
+	select region_id as r_id, 'Americas', country_id, name
+	from country
+	where region_id = 2
+union
+	select region_id as r_id, 'Asia', country_id, name
+	from country
+	where region_id = 3
+union
+	select region_id as r_id, 'Middle East and Africa', country_id, name
+	from country
+	where region_id = 4
+order by r_id, name;
+
+-- The join is much more compact and readable
+select r.region_id as r_id, r.name as region, c.country_id, c.name
+from country c join region r
+using(region_id)
+order by r_id, name;
