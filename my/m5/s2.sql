c31c6462de00f45825e3e097b0d915747c6feecb
Emanuele <egalli64@gmail.com>
2024-07-31 11:25:26 +0200

    more info

--- my/m5/s2.sql
+++ my/m5/s2.sql
@@ -7,16 +7,17 @@
  */
 use hron;
 
--- check the current table status
+-- check the current table structure
 describe service;
 
+-- check the current table values (and its associated city in the location table)
 select s.*, l.city
 from service s left outer join location l
 using (location_id);
 
 -- plain insert
 insert into service (service_id, name, location_id) values
-	(202, 'Mayday Shifty Solutions', 2);
+	(20, 'Mayday Shifty Solutions', 2);
 
 -- multiline insert
 insert into service (service_id, name, location_id) values
