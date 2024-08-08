2ec7933592d2a36d8b392695ae685e5ce28c4ea8
Emanuele <egalli64@gmail.com>
2024-08-08 22:12:08 +0200

    solution

--- exercises/murder_mistery.sql
+++ exercises/murder_mistery.sql
@@ -53,10 +53,7 @@ from person
 where address_street_name = 'Franklin Ave' and name like 'Annabel %';
 -- 16371
 
-select *
-from interview
-where person_id = 14887
-
+-- Task 3: Info from witnesses
 select p.name, i.transcript
 from person p join interview i
 on p.id = i.person_id
@@ -70,3 +67,114 @@ where id in (14887, 16371);
 -- Annabel Miller
 -- I saw the murder happen, and I recognized the killer from my gym
 -- when I was working out last week on January the 9th.
+
+-- Task 4: info on Get Fit Now members
+select sql
+from sqlite_master
+where name = 'get_fit_now_member';
+-- CREATE TABLE get_fit_now_member ( id text PRIMARY KEY, person_id integer, name text,
+-- membership_start_date integer, membership_status text,
+-- FOREIGN KEY (person_id) REFERENCES person(id) )
+
+-- Task 4a: possible killers according to Shapiro
+select name
+from get_fit_now_member
+where membership_status = 'gold' and id like '48Z%';
+-- Joe Germuska
+-- Jeremy Bowers
+
+-- Task 4b: Annabel Miller id
+select id
+from get_fit_now_member
+where name = 'Annabel Miller';
+-- 90081
+
+-- Task 5: info on check_in
+select sql
+from sqlite_master
+where name = 'get_fit_now_check_in';
+-- CREATE TABLE get_fit_now_check_in ( membership_id text, check_in_date integer,
+-- check_in_time integer, check_out_time integer,
+-- FOREIGN KEY (membership_id) REFERENCES get_fit_now_member(id) )
+
+-- Task 5a: check in time for Annabel Miller in the stated day
+select check_in_time, check_out_time
+from get_fit_now_check_in ci join get_fit_now_member m 
+on ci.membership_id = m.id
+where ci.check_in_date = 20180109 and m.name = 'Annabel Miller'; 
+-- 1600	1700
+
+-- Task 5b: possible killers according to Miller
+select m.name
+from get_fit_now_check_in ci join get_fit_now_member m 
+on ci.membership_id = m.id
+where ci.check_in_date = 20180109
+	and check_in_time <= 1700
+	and check_out_time >= 1600
+	and name != 'Annabel Miller';
+-- Joe Germuska
+-- Jeremy Bowers
+
+-- Task 6: info on drivers_license
+select sql
+from sqlite_master
+where name = 'drivers_license';
+-- CREATE TABLE drivers_license ( id integer PRIMARY KEY, age integer, height integer,
+-- eye_color text, hair_color text, gender text, plate_number text, car_make text, car_model text )
+
+-- Task 6a: possible killers according to Shapiro
+select p.name
+from drivers_license dl join person p
+on dl.id = p.license_id
+where gender = 'male' and plate_number like '%H42W%';
+-- Tushar Chandra
+-- Jeremy Bowers
+
+-- Task 7: combine the partial results
+	select p.name
+	from drivers_license dl join person p
+	on dl.id = p.license_id
+	where gender = 'male' and plate_number like '%H42W%'
+intersect
+	select m.name
+	from get_fit_now_check_in ci join get_fit_now_member m 
+	on ci.membership_id = m.id
+	where ci.check_in_date = 20180109
+		and check_in_time <= 1700
+		and check_out_time >= 1600
+		and name != 'Annabel Miller'
+intersect
+	select name
+	from get_fit_now_member
+	where membership_status = 'gold' and id like '48Z%';
+-- Jeremy Bowers
+
+INSERT INTO solution VALUES (1, 'Jeremy Bowers');
+
+
+-- Task 8: extra - the brains behind the murder
+select i.transcript
+from interview i join person p
+on on p.id = i.person_id
+where name = 'Jeremy Bowers';
+-- I was hired by a woman with a lot of money. 
+-- I don't know her name but I know she's around 5'5" (65") or 5'7" (67").
+-- She has red hair and she drives a Tesla Model S.
+-- I know that she attended the SQL Symphony Concert 3 times in December 2017. 
+
+	select p.name
+	from facebook_event_checkin fb join person p
+	on fb.person_id = p.id
+	where fb.event_name like 'SQL Symphony Concert'
+		and date like '201712%'
+	group by fb.person_id
+	having count(*) = 3
+intersect
+	select p.name
+	from drivers_license dl join person p
+	on p.license_id = dl.id
+	where gender = 'female'
+		and height between 65 and 67
+		and hair_color = 'red'
+		and car_make = 'Tesla' and car_model = 'Model S'
+-- Miranda Priestly
