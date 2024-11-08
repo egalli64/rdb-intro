4cdf2d19127daa797dc1f688aeb7484ca9378beb
Emanuele <egalli64@gmail.com>
2024-11-08 22:48:35 +0100

    review

--- my/m2/s08.sql
+++ my/m2/s08.sql
@@ -7,6 +7,10 @@
  */
 use hron;
 
+-- a cross join could be done on whichever couple of tables (better if they are small ones)
+select * from service;
+select * from region;
+
 -- cartesian product - following the SQL/92 syntax
 select r.name, s.name
 from region r cross join service s;
