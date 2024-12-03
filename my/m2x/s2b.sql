c01c337dec4d0e023be51c3b6be2c3e0169799f4
Emanuele <egalli64@gmail.com>
2024-12-03 21:36:47 +0100

    review

--- my/m2x/s2b.sql
+++ my/m2x/s2b.sql
@@ -9,34 +9,34 @@ use hron;
 
 -- Left join for department 12 or 'Grant' -> Treasury is missing
 select e.first_name, e.last_name, d.name, d.department_id
-from employee e left outer join department d
-using (department_id)
+from employee e
+left outer join department d using (department_id)
 where d.department_id = 12 or e.last_name = 'Grant';
 
 -- Right join for department 12 or 'Grant' -> Kimberely is missing
 select e.first_name, e.last_name, d.name, d.department_id
-from employee e right outer join department d
-using (department_id)
+from employee e
+right outer join department d using (department_id)
 where d.department_id = 12 or e.last_name = 'Grant';
 
 -- Emulation of a FULL OUTER JOIN
 	select e.first_name, e.last_name, d.name, d.department_id
-	from employee e left outer join department d
-	using (department_id)
+	from employee e
+	left outer join department d using (department_id)
 	where d.department_id = 12 or e.last_name = 'Grant'
 union
 	select e.first_name, e.last_name, d.name, d.department_id
-	from employee e right outer join department d
-	using (department_id)
+	from employee e
+	right outer join department d using (department_id)
 	where d.department_id = 12 or e.last_name = 'Grant';
 
 -- Union all keeps duplicated rows
 	select e.first_name, e.last_name, d.name, d.department_id
-	from employee e left outer join department d
-	using (department_id)
+	from employee e
+	left outer join department d using (department_id)
 	where d.department_id = 12 or e.last_name = 'Grant'
 union all
 	select e.first_name, e.last_name, d.name, d.department_id
-	from employee e right outer join department d
-	using (department_id)
+	from employee e
+	right outer join department d using (department_id)
 	where d.department_id = 12 or e.last_name = 'Grant';
