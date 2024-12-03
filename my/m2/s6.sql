acdc91f0ded5bb3b3df229f805971ce2144ae21d
Emanuele <egalli64@gmail.com>
2024-12-03 18:24:10 +0100

    review

--- my/m2/s6.sql
+++ my/m2/s6.sql
@@ -3,7 +3,7 @@
     
     https://github.com/egalli64/hron
 
-    Examples left/right outer join - from "many" perspective
+    Examples left/right outer join - from the "many" perspective
  */
 use hron;
 
@@ -15,25 +15,24 @@ where last_name like 'Gr%';
 
 -- the inner join skips Kimberely
 select e.first_name, e.last_name, d.name as department
-from employee e inner join department d
--- using (department_id)
-on e.department_id = d.department_id
+from employee e
+inner join department d on e.department_id = d.department_id -- or: using (department_id)
 where e.last_name like 'Gr%';
 
 -- left outer join preserves Kimberely, giving NULL for the department columns
 select e.first_name, e.last_name, d.name as department
-from employee e left outer join department d
-using (department_id)
+from employee e
+left outer join department d using (department_id)
 where e.last_name like 'Gr%';
 
 -- rewritten as right outer join
 select e.first_name, e.last_name, d.name as department
-from department d right outer join employee e
-using (department_id)
+from department d
+right outer join employee e using (department_id)
 where e.last_name like 'Gr%';
 
 -- "left" or "right" implies "outer"
 select e.first_name, e.last_name, d.name as department
-from employee e left join department d
-using (department_id)
+from employee e
+left join department d using (department_id)
 where e.last_name = 'Grant';
