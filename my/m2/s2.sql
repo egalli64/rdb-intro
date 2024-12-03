596f09866b5227778af229cd30bb8640d56638a5
Emanuele <egalli64@gmail.com>
2024-12-03 16:46:47 +0100

    review

--- my/m2/s2.sql
+++ my/m2/s2.sql
@@ -7,11 +7,14 @@
  */
 use hron;
 
--- name of a region and name of countries for that region
+-- 1. Without join, get the required information in many resultsets
+
+-- 1a. name of a region
 select name
 from region
 where region_id = 1;
 
+-- 1b. name of a region of countries for that region
 select name
 from country
 where region_id = 1;
@@ -25,12 +28,14 @@ select name
 from country
 where region_id = 4;
 
--- join the two tables and see the full result in a single shot
+-- 2. With join, get a single resultset with all the required information
+
+-- Join in the "classic" way
 select region.region_id as 'Region Id', region.name, country_id, country.name
 from region, country
 where region.region_id = country.region_id;
 
--- using table (and column) aliases
+-- Using table (and column) aliases make query and resultset more readable
 select r.region_id as 'Region Id', r.name as 'Region Name', c.name as Country
 from region r, country c
 where r.region_id = c.region_id;
