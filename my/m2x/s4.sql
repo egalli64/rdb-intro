5c39928ab85d97bccc0b5610f75215b6d82b9f6e
Emanuele <egalli64@gmail.com>
2024-12-04 16:09:27 +0100

    review

---
+++ my/m2x/s4.sql
@@ -0,0 +1,20 @@
+/*
+    Introduction to SQL (on MySQL)
+    
+    https://github.com/egalli64/hron
+
+    Examples on self join
+ */
+use hron;
+
+-- SQL/92
+select concat(e.first_name, ' ', e.last_name) as Employee, concat(m.first_name, ' ', m.last_name) as Manager
+from employee e
+join employee m on e.manager_id = m.employee_id
+order by Employee;
+
+-- "classic"
+select concat(e.first_name, ' ', e.last_name) as Employee, concat(m.first_name, ' ', m.last_name) as Manager
+from employee e, employee m
+where e.manager_id = m.employee_id
+order by Employee;
