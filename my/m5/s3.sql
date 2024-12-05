969ec612870c44fba211111fc5d7a2179cc53512
Emanuele <egalli64@gmail.com>
2024-12-05 22:52:07 +0100

    review

--- my/m5/s3.sql
+++ my/m5/s3.sql
@@ -6,44 +6,50 @@
     Examples on update
  */
 use hron;
-
--- check the current table structure
-describe service;
+set autocommit = OFF;
 
 -- check the current table values (and its associated city in the location table)
 select s.*, l.city
 from service s left outer join location l using (location_id);
 
+-- insert a stub record to be completed in a second moment
+insert into service (location_id)
+values (21), (22);
+
 -- updating a single row (if found)
 update service
-set name = right(name, 2), location_id = 3
-where service_id = 20;
+set name = 'Missing', location_id = 22
+where location_id = 21;
 
 -- updating (potentially) more rows
 update service
 set name = 'N/A'
-where service_id > 10;
+where location_id = 22;
 
--- updating all rows - by mistake?
--- ??? committing ???
+-- ??? updating all rows - by mistake ???
+--- when not in autocommit mode, we can rollback to the original state
 update service
 set name = 'mistake!?';
 
--- you can choose between committing or rolling back
-rollback;
-commit;
+--
+-- UPDATE with a subquery
+--
+-- We want to increase by 100 the salary of each employee having as manager
+-- the one associated to the department IT
+--
 
--- check the current salary for IT people
-select e.first_name, e.last_name, e.salary, e.manager_id
-from employee e join department d using (department_id)
-where d.name = 'IT';
-
--- before building a subquery could be a good idea to test it as a stand-alone querys
+-- 1. get the manager id from department
 select manager_id
 from department
 where name = 'IT';
 
--- update - where subquery
+-- 2. check the employees in the IT department
+select e.first_name, e.last_name, e.salary, e.manager_id
+from employee e
+join department d using (department_id)
+where d.name = 'IT';
+
+-- 3. update - where subquery
 -- assuming department name is not duplicated!
 update employee
 set salary = salary + 100
@@ -52,3 +58,6 @@ where manager_id = (
 	from department
 	where name = 'IT'
 );
+
+-- get back to the original salaries
+rollback;
