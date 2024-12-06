0cb9eace5fa273035497e51354583d0b8341691e
Emanuele <egalli64@gmail.com>
2024-12-06 23:09:58 +0100

    review

--- my/m6/s4.sql
+++ my/m6/s4.sql
@@ -3,46 +3,107 @@
     
     https://github.com/egalli64/hron
 
-    Examples on transaction anomalies -- see s7T2.sql for the other transaction code
+    Examples on transaction anomalies -- see s4T2.sql for the other transaction code
  */
 use hron;
 
+--
 -- phantom read
-start transaction; -- (1)
-	-- (2)
-	select *
-	from service;
-	
-	-- (3) executed by T2
-	-- insert into service (service_id, name) values (22, 'T2 insert');
-	
- 	-- (4) executed by T2
- 	-- commit;
- 	
-	-- (5) if T1 sees a change here, it is phantom read!
-	select *
-	from service;
-commit; -- (6) executed by T1
+--
 
+-- (1)
+start transaction;
+
+-- (2)
+select *
+from service;
+	
+-- (3) executed by T2
+-- insert into service (service_id, name) values (22, 'T2 insert');
+
+-- (4) executed by T2
+-- commit;
+	
+-- (5) if T1 sees a change here, it is phantom read!
+select *
+from service;
+
+-- (6) executed by T1
+commit;
+
+--
 -- non-repeatable read
-start transaction; -- (1)
-	select * from service where service_id = 22; -- (2)
-	-- update service set name = 'T2 update' where service_id = 22; -- (3) T2 update the row
-	-- commit; -- (4) executed by T2
-	select * from service where service_id = 22; -- (5) if T1 sees a change here, it is a non-repeatable read!
-commit; -- (6) executed by T1
-select * from service where service_id = 22; -- (7) both T1 and T2 see the change
+--
 
+-- (1)
+start transaction;
+
+-- (2)
+select *
+from service
+where service_id = 22;
+
+-- (3) T2 update the row
+-- update service set name = 'T2 update' where service_id = 22;
+
+-- (4) executed by T2
+-- commit;
+
+-- (5) if T1 sees a change here, it is a non-repeatable read!
+select *
+from service
+where service_id = 22;
+
+-- (6) executed by T1
+commit;
+
+-- (7) both T1 and T2 see the change
+select *
+from service
+where service_id = 22;
+
+--
 -- lost update
-start transaction; -- (1)
-	update service set name = concat(name, 'X') where service_id = 22; -- (2) T1
-	-- update service set name = concat(name, 'Z') where service_id = 22; -- (3) T2 expected to hang until T1 commit
-commit; -- (4)
-select * from service where service_id = 22; -- (5) both X and Z, or there has been a lost update!
+--
 
+-- (1)
+start transaction;
+
+-- (2) T1
+update service
+set name = concat(name, 'X')
+where service_id = 22;
+
+-- (3) T2 expected to hang until T1 commit
+-- update service set name = concat(name, 'Z') where service_id = 22;
+
+-- (4)
+commit;
+
+-- (5) both X and Z, or there has been a lost update!
+select *
+from service
+where service_id = 22;
+
+--
 -- dirty read
-start transaction; -- (1)
-	-- update service set name = 'dirty' where service_id = 22; -- (2) T2
-	select * from service where service_id = 22; -- (3) if both T1 and T2 read the changed value, this is a dirty read!
-rollback; -- (4)
-select * from service where service_id = 22; -- (5) back to previous value
+--
+
+-- (1)
+start transaction;
+
+-- (2) T2
+-- update service set name = 'dirty' where service_id = 22;
+
+-- (3) if both T1 and T2 read the changed value, this is a dirty read!
+select *
+from service
+where service_id = 22;
+
+-- (4)
+rollback;
+
+-- (5) back to previous value
+select *
+from service
+where service_id = 22;
